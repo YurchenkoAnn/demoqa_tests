108e971e5dddfdc061d37dc2a45c7ead2a2bd181
YurchenkoAnn <anka.cyclik4321@gmail.com>
2022-05-29 19:06:35 +0500

    added first test- open url

---
+++ src/test/java/RegistrationForm.java
@@ -0,0 +1,16 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationForm {
+    @Test
+    void successfulTest() {
+
+        open("https://demoqa.com/automation-practice-form/");
+    }
+}
