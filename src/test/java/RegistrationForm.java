a95314cd005321a44312c46d6a4844c721c0f99d
YurchenkoAnn <anka.cyclik4321@gmail.com>
2022-05-29 19:19:46 +0500

    added text forms

--- src/test/java/RegistrationForm.java
+++ src/test/java/RegistrationForm.java
@@ -10,7 +10,22 @@ import static com.codeborne.selenide.Selenide.open;
 public class RegistrationForm {
     @Test
     void successfulTest() {
+        String firstname = "Иван";
+        String lastName = "Иванов";
+        String number = "2-342-223";
+        String email = "ivanov@egorov.com";
+        String address = "Иванова 1б";
 
         open("https://demoqa.com/automation-practice-form/");
+
+        $("[id=firstName]").setValue(firstname);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=userEmail]").setValue(email);
+        $("[id=userNumber]").setValue(number);
+        $("[id=currentAddress]").setValue(address);
+
+        //$("[id=submit]").click();
+
+
     }
 }
